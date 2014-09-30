1f5107e4bc4a09a4456568c7ded93bac3647f76a
James Shore <jshore@jamesshore.com>
2014-09-29 17:44:19 -0700

    Upgraded Safari 6.1.6 --> 6.2.0

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -10,6 +10,6 @@ module.exports = [
 	"IE 11.0.0 (Windows 7)",
 	"Firefox 32.0.0 (Mac OS X 10.8)",
 	"Chrome 37.0.2062 (Mac OS X 10.8.5)",
-	"Safari 6.1.6 (Mac OS X 10.8.5)",
+	"Safari 6.2.0 (Mac OS X 10.8.5)",
 	"Mobile Safari 7.0.0 (iOS 7.1)"
 ];
