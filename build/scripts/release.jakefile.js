4143d544a2c312fe37d4028600cf5512f47798e1
James Shore <jshore@jamesshore.com>
2014-09-30 16:45:59 -0700

    Major version releases can't happen yet

--- build/scripts/release.jakefile.js
+++ build/scripts/release.jakefile.js
@@ -12,7 +12,16 @@ var INTEGRATION_BRANCH = "master";
 
 //*** DO THE RELEASE
 
-desc("Increment 'bugfix' version number and release");
+task("default", function() {
+	console.log("Use 'major', 'minor', or 'bugfix' to perform release");
+});
+
+desc("Increment major version number and release");
+task("major", function() {
+	fail("Major version releases are disabled while we're in 0.x releases.");
+});
+
+desc("Increment bugfix version number and release");
 task("bugfix", [], function() {
 	console.log("\n\nRELEASE OK");
 });
