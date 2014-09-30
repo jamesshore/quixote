78a98ac23723dabc1140afdfc8bccbf07de35071
James Shore <jshore@jamesshore.com>
2014-09-30 12:27:44 -0700

    Factored out 'readyToIntegrate' target

--- build/scripts/integrate.jakefile.js
+++ build/scripts/integrate.jakefile.js
@@ -11,23 +11,36 @@ var DEV_BRANCH = "dev";
 //*** GENERAL
 
 desc("Integrate latest development code into known-good branch");
-task("default", [ "devBranch", "allCommitted", "build" ], function() {
+task("default", [ "integrate" ], function() {
 	console.log("\n\nINTEGRATION OK");
 });
 
-task("devBranch", function() {
+task("integrate", [ "readyToIntegrate", "integrationBranch" ], function() {
+
+});
+
+task("integrationBranch", function() {
+	console.log("Switching to integration branch: .");
+});
+
+//*** ENSURE INTEGRATION READINESS
+
+//task("readyToIntegrate", [ "onDevBranch", "allCommitted", "buildsClean" ]);
+task("readyToIntegrate", [], function() {
+	console.log("*** STUBBED! readyToIntegrate");
+});
+
+task("onDevBranch", function() {
 	console.log("Checking git branch: .");
 	git.checkBranch(DEV_BRANCH, complete, fail);
 }, { async: true });
 
 task("allCommitted", function() {
 	console.log("Checking git repository for uncommitted files: .");
-	console.log("  STUBBED");
-	return complete();
-//	git.checkNothingToCommit(complete, fail);
+	git.checkNothingToCommit(complete, fail);
 }, { async: true });
 
-task("build", function() {
+task("buildsClean", function() {
 	console.log("Verifying build:");
 	jake.exec(BUILD_COMMAND, { printStdout: true, printStderr: true }, complete);
 }, { async: true });
