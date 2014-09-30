aab0a164f010f859d99a8c9c2af6c38bebcf684d
James Shore <jshore@jamesshore.com>
2014-09-30 10:52:24 -0700

    Factored out runGit()

--- build/util/git_runner.js
+++ build/util/git_runner.js
@@ -4,30 +4,31 @@
 var spawn = require("child_process").spawn;
 
 exports.checkBranch = function(expectedBranch, success, fail) {
-	var git = spawn("git", [ "symbolic-ref", "HEAD" ], { stdio: [ "ignore", "pipe", process.stderr ] });
+	runGit("symbolic-ref HEAD", function(err, stdout) {
+		if (err) return fail(err);
 
+		console.log("OUTPUT: " + stdout);
+		return success();
+	});
+};
+
+function runGit(args, callback) {
+	var git = spawn("git", args.split(" "), { stdio: [ "ignore", "pipe", process.stderr ] });
 
 	var output = "";
+	var callbackAlreadyCalled = false;
 
 	git.stdout.setEncoding("utf8");
 	git.stdout.on("data", function(data) {
-		console.log("DATA: " + data);
 		output += data;
 	});
-
-
-
-	var callbackAlreadyCalled = false;
 
 	git.on("error", function(error) {
 		doCallback("Problem running git: " + error);
 	});
-	git.once("exit", function(code, signal) {
+	git.on("exit", function(code, signal) {
 		if (signal) return doCallback("git exited in response to signal: " + signal);
 		if (code) return doCallback("git exited with error code " + code);
-
-
-		console.log("OUTPUT: " + output);
 
 		return doCallback();
 	});
@@ -36,8 +37,8 @@ exports.checkBranch = function(expectedBranch, success, fail) {
 		if (callbackAlreadyCalled) return;
 		callbackAlreadyCalled = true;
 
-		if (error) fail(error);
-		else success();
+		if (error) callback(error);
+		else callback(null, output);
 	}
-};
 
+}
