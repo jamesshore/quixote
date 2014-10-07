1740887a233782391f90941bc2d2811c646aac07
James Shore <jshore@jamesshore.com>
2014-10-07 16:47:10 -0700

    Type check assertions removed from ensure.js
    Use ensure.signature() instead

--- src/util/_ensure_test.js
+++ src/util/_ensure_test.js
@@ -149,65 +149,6 @@ describe("Ensure", function() {
 
 	});
 
-	describe("type checking", function() {
-
-		it("checks if variable is defined", function() {
-			var defined = wrap(ensure.defined);
-
-			assert.noException(defined("foo"));
-			assert.noException(defined(null));
-			assert.exception(defined(undefined), /variable was not defined/);
-			assert.exception(defined(undefined, "myVariable"), /myVariable was not defined/);
-		});
-
-		it("checks if variable is number, string, or array", function() {
-			var number = wrap(ensure.number);
-			var string = wrap(ensure.string);
-			var array = wrap(ensure.array);
-			var fn = wrap(ensure.fn);
-
-			assert.noException(number(0));
-			assert.exception(number("foo"), /variable expected number, but was string/);
-			assert.exception(number({}), /variable expected number, but was object/);
-			assert.exception(number([]), /variable expected number, but was array/);
-			assert.exception(number(undefined), /variable expected number, but was undefined/);
-			assert.exception(number(null), /variable expected number, but was null/);
-			assert.exception(number(NaN), /variable expected number, but was NaN/);
-
-			assert.exception(number("foo", "name"), /name expected number, but was string/);
-
-			assert.exception(string(null, "name"), /name expected string, but was null/);
-			assert.exception(array(null, "name"), /name expected array, but was null/);
-			assert.exception(fn(null, "name"), /name expected function, but was null/);
-		});
-
-		it("checks if variable is object of specific type", function() {
-			function Example1() {}
-
-			function Example2() {}
-
-			function NoConstructor() {}
-
-			delete NoConstructor.constructor;
-			var Anon = function() {};
-			var object = wrap(ensure.object);
-
-			assert.noException(object(new Example1()));
-			assert.noException(object(new Example1(), Example1));
-
-			assert.exception(object(null, Example1), /variable expected object, but was null/);
-			assert.exception(object(new Example1(), Example2), /Expected variable to be (Example2 instance|a specific type)(, but was Example1)?/);
-			assert.exception(object(new Anon(), Example2), /Expected variable to be (Example2 instance|a specific type)/);
-			assert.exception(object(new NoConstructor(), Example2), /Expected variable to be (Example2 instance|a specific type)/);
-			assert.exception(object(new Example1(), Example2, "name"), /Expected name to be (Example2 instance|a specific type)/);
-
-			if (Object.create) {    // don't run this test on IE 8
-				assert.exception(object(Object.create(null), Example2), /Expected variable to be (Example2 instance|a specific type), but it has no prototype/);
-			}
-		});
-
-	});
-
 	function wrap(fn) {
 		return function() {
 			var outerArgs = arguments;
--- src/util/ensure.js
+++ src/util/ensure.js
@@ -14,12 +14,6 @@ exports.unreachable = function(message) {
 	throw new EnsureException(exports.unreachable, message);
 };
 
-
-
-exports.defined = function(variable, variableName) {
-	if (variable === undefined) throw new EnsureException(exports.defined, normalize(variableName) + " was not defined");
-};
-
 exports.signature = function(args, signature) {
 	signature = signature || [];
 	var expectedArgCount = signature.length;
@@ -47,106 +41,62 @@ exports.signature = function(args, signature) {
 			);
 		}
 	}
-
-	function typeMatches(type, arg) {
-		for (var i = 0; i < type.length; i++) {
-			if (oneTypeMatches(type[i], arg)) return true;
-		}
-		return false;
-
-		function oneTypeMatches(type, arg) {
-			switch (getType(arg)) {
-				case "boolean": return type === Boolean;
-				case "string": return type === String;
-				case "number": return type === Number;
-				case "array": return type === Array;
-				case "function": return type === Function;
-				case "object": return arg instanceof type;
-				case "undefined": return type === undefined;
-				case "null": return type === null;
-				case "NaN": return isNaN(type);
-
-				default: exports.unreachable();
-			}
-		}
-	}
-
-	function explainType(type) {
-		var joiner = "";
-		var result = "";
-		for (var i = 0; i < type.length; i++) {
-			result += joiner + explainOneType(type[i]);
-			joiner = (i === type.length - 2) ? ", or " : ", ";
-		}
-		return result;
-
-		function explainOneType(type) {
-			switch (type) {
-				case Boolean: return "boolean";
-				case String: return "string";
-				case Number: return "number";
-				case Array: return "array";
-				case Function: return "function";
-				case null: return "null";
-				default:
-					if (typeof type === "number" && isNaN(type)) return "NaN";
-					else return (type.name || "<anon>") + " instance";
-			}
-		}
-	}
-
-	function explainArg(arg) {
-		var type = getType(arg);
-		if (type !== "object") return type;
-
-		var prototype = Object.getPrototypeOf(arg);
-		if (prototype === null) return "an object without a prototype";
-		else return (prototype.constructor.name || "<anon>") + " instance";
-	}
 };
 
-
-exports.boolean = checkTypeFn("boolean");
-exports.string = checkTypeFn("string");
-exports.number = checkTypeFn("number");
-exports.array = checkTypeFn("array");
-exports.fn = checkTypeFn("function");
-
-exports.object = function(variable, constructor, variableName) {
-	checkTypeFn("object")(variable, variableName);
-	if (constructor === undefined) return;
-
-	if (!(variable instanceof constructor)) {
-		var variablePrototype = Object.getPrototypeOf(variable);
-		var constructorType = constructor.name;
-		var variableType;
-
-		constructorType = constructorType ? constructorType + " instance" : "a specific type";
-		if (variablePrototype === null) {
-			variableType = ", but it has no prototype";
-		}
-		else {
-			variableType = variablePrototype.constructor.name;
-			variableType = variableType ? ", but was " + variableType : "";
-		}
-
-		throw new EnsureException(
-			exports.object,
-			"Expected " + normalize(variableName) + " to be " + constructorType + variableType
-		);
+function typeMatches(type, arg) {
+	for (var i = 0; i < type.length; i++) {
+		if (oneTypeMatches(type[i], arg)) return true;
 	}
-};
+	return false;
 
-function checkTypeFn(expectedType) {
-	return function(variable, variableName) {
-		var actualType = getType(variable);
-		if (actualType !== expectedType) {
-			throw new EnsureException(
-				exports.number,
-				normalize(variableName) + " expected " + expectedType + ", but was " + actualType
-			);
+	function oneTypeMatches(type, arg) {
+		switch (getType(arg)) {
+			case "boolean": return type === Boolean;
+			case "string": return type === String;
+			case "number": return type === Number;
+			case "array": return type === Array;
+			case "function": return type === Function;
+			case "object": return arg instanceof type;
+			case "undefined": return type === undefined;
+			case "null": return type === null;
+			case "NaN": return isNaN(type);
+
+			default: exports.unreachable();
 		}
-	};
+	}
+}
+
+function explainType(type) {
+	var joiner = "";
+	var result = "";
+	for (var i = 0; i < type.length; i++) {
+		result += joiner + explainOneType(type[i]);
+		joiner = (i === type.length - 2) ? ", or " : ", ";
+	}
+	return result;
+
+	function explainOneType(type) {
+		switch (type) {
+			case Boolean: return "boolean";
+			case String: return "string";
+			case Number: return "number";
+			case Array: return "array";
+			case Function: return "function";
+			case null: return "null";
+			default:
+				if (typeof type === "number" && isNaN(type)) return "NaN";
+				else return (type.name || "<anon>") + " instance";
+		}
+	}
+}
+
+function explainArg(arg) {
+	var type = getType(arg);
+	if (type !== "object") return type;
+
+	var prototype = Object.getPrototypeOf(arg);
+	if (prototype === null) return "an object without a prototype";
+	else return (prototype.constructor.name || "<anon>") + " instance";
 }
 
 function getType(variable) {
@@ -157,10 +107,8 @@ function getType(variable) {
 	return type;
 }
 
-function normalize(variableName) {
-	return variableName ? variableName : "variable";
-}
 
+/*****/
 
 var EnsureException = exports.EnsureException = function(fnToRemoveFromStackTrace, message) {
 	if (Error.captureStackTrace) Error.captureStackTrace(this, fnToRemoveFromStackTrace);
@@ -169,7 +117,6 @@ var EnsureException = exports.EnsureException = function(fnToRemoveFromStackTrac
 EnsureException.prototype = createObject(Error.prototype);
 EnsureException.prototype.constructor = EnsureException;
 EnsureException.prototype.name = "EnsureException";
-
 
 // WORKAROUND IE8: shim Object.create()
 function createObject(prototype) {
