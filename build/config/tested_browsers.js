8cbe16aa661c6d9b7298688561b5c7b1dfef1152
James Shore <jshore@jamesshore.com>
2014-10-07 15:22:47 -0700

    Chrome 37.0.2062 --> 38.0.2125

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -10,7 +10,7 @@ module.exports = [
 	"IE 10.0.0 (Windows 7)",
 	"IE 11.0.0 (Windows 7)",
 	"Firefox 32.0.0 (Mac OS X 10.8)",
-	"Chrome 37.0.2062 (Mac OS X 10.8.5)",
+	"Chrome 38.0.2125 (Mac OS X 10.8.5)",
 	"Safari 6.2.0 (Mac OS X 10.8.5)",
 	"Mobile Safari 7.0.0 (iOS 7.1)"
 ];
