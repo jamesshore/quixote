9a9a5b6b879be1e5569ec507cef4eedf6a3a00cf
James Shore <jshore@jamesshore.com>
2014-10-15 10:35:01 -0700

    YPosition class

---
+++ src/values/_y_position_test.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2014 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+"use strict";
+
+var assert = require("../util/assert.js");
+var YPosition = require("./y_position.js");
+
+
+describe.only("Y Position", function() {
+
+	it("converts to string", function() {
+		var y1 = new YPosition(10);
+
+		assert.equal(y1.toString(), "10px");
+	});
+
+	it("checks equality", function() {
+		var y1a = new YPosition(10);
+		var y1b = new YPosition(10);
+		var y2 = new YPosition(20);
+
+		assert.objEqual(y1a, y1b, "same");
+		assert.objNotEqual(y1a, y2, "different");
+	});
+
+});
---
+++ src/values/y_position.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2014 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+"use strict";
+
+var ensure = require("../util/ensure.js");
+
+var Me = module.exports = function YPosition(position) {
+	ensure.signature(arguments, [ Number ]);
+
+	this._position = position;
+};
+
+Me.prototype.toString = function() {
+	ensure.signature(arguments, []);
+
+	return this._position + "px";
+};
+
+Me.prototype.equals = function(that) {
+	ensure.signature(arguments, [ Me ]);
+
+	return this._position === that._position;
+};
