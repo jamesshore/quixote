e0cbd38eb41822d6b1211221c2d35f97856c2fca
James Shore <jshore@jamesshore.com>
2014-10-31 16:36:17 -0700

    ElementCenter implementation is position-agnostic

--- src/descriptors/element_center.js
+++ src/descriptors/element_center.js
@@ -9,16 +9,17 @@ var RelativePosition = require("./relative_position.js");
 var X_DIMENSION = "x";
 var Y_DIMENSION = "y";
 
-var Me = module.exports = function ElementCenter(dimension, element) {
-	var QElement = require("../q_element.js");    // break circular dependency
-	ensure.signature(arguments, [ String, QElement ]);
+var Me = module.exports = function ElementCenter(dimension, element1, element2, description) {
+	ensure.signature(arguments, [ String, PositionDescriptor, PositionDescriptor, String ]);
 
 	if (dimension === X_DIMENSION) PositionDescriptor.x(this);
 	else if (dimension === Y_DIMENSION) PositionDescriptor.y(this);
 	else ensure.unreachable("Unknown dimension: " + dimension);
 
 	this._dimension = dimension;
-	this._element = element;
+	this._element1 = element1;
+	this._element2 = element2;
+	this._description = description;
 };
 PositionDescriptor.extend(Me);
 
@@ -27,22 +28,17 @@ Me.y = factoryFn(Y_DIMENSION);
 
 Me.prototype.value = function value() {
 	ensure.signature(arguments, []);
-
-	var position = this._element.getRawPosition();
-
-	if (this._dimension === X_DIMENSION) return Position.x(position.left + ((position.right - position.left) / 2));
-	else return Position.y(position.top + ((position.bottom - position.top) / 2));
+	return this._element1.value().midpoint(this._element2.value());
 };
 
 Me.prototype.toString = function toString() {
 	ensure.signature(arguments, []);
-
-	var description = (this._dimension === X_DIMENSION) ? "center" : "middle";
-	return description + " of " + this._element;
+	return this._description;
 };
 
 function factoryFn(dimension) {
 	return function(element) {
-		return new Me(dimension, element);
+		if (dimension === X_DIMENSION) return new Me(dimension, element.left, element.right, "center of " + element);
+		else return new Me(dimension, element.top, element.bottom, "middle of " + element);
 	};
 }
