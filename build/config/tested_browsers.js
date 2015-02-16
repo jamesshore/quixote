cd359ba277d39fb0ed0942a937a32c72cb86d9b4
James Shore <jshore@jamesshore.com>
2015-02-16 14:06:03 -0800

    Add Opera to list of tested browsers

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -5,14 +5,15 @@
 // browser + operating system combinations.
 
 module.exports = [
+	"Chrome 40.0.2214 (Mac OS X 10.10.2)",
+	"Chrome 40.0.2214 (Windows 7)",
+	"Firefox 35.0.0 (Mac OS X 10.10)",
+	"Firefox 33.0.0 (Windows 7)",
 	"IE 8.0.0 (Windows 7)",
 	"IE 9.0.0 (Windows 7)",
 	"IE 10.0.0 (Windows 7)",
 	"IE 11.0.0 (Windows 7)",
-	"Firefox 35.0.0 (Mac OS X 10.10)",
-	"Firefox 33.0.0 (Windows 7)",
-	"Chrome 40.0.2214 (Mac OS X 10.10.2)",
-	"Chrome 40.0.2214 (Windows 7)",
 	"Safari 8.0.3 (Mac OS X 10.10.2)",
-	"Mobile Safari 7.0.0 (iOS 7.1)"
+	"Mobile Safari 7.0.0 (iOS 7.1)",
+	"Opera 27.0.1689 (Mac OS X 10.10.2)"
 ];
