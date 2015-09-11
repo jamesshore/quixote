2aa59ee0a2c2f77ff337780e6fe248749d8fec2e
James Shore <jshore@jamesshore.com>
2015-09-11 15:13:27 -0700

    Upgraded Mac OS 10.10.3 --> 10.10.5

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -5,13 +5,13 @@
 // browser + operating system combinations.
 
 module.exports = [
-	"Chrome 45.0.2454 (Mac OS X 10.10.3)",
+	"Chrome 45.0.2454 (Mac OS X 10.10.5)",
 	"Firefox 40.0.0 (Mac OS X 10.10)",
 	"IE 8.0.0 (Windows 7)",
 	"IE 9.0.0 (Windows 7)",
 	"IE 10.0.0 (Windows 7)",
 	"IE 11.0.0 (Windows 7)",
-	"Safari 8.0.6 (Mac OS X 10.10.3)",
+	"Safari 8.0.6 (Mac OS X 10.10.5)",
 	"Mobile Safari 7.0.0 (iOS 7.1)",
-	"Opera 27.0.1689 (Mac OS X 10.10.3)"
+	"Opera 27.0.1689 (Mac OS X 10.10.5)"
 ];
