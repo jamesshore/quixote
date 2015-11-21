cf2fb6b2005d1dd381f66042cd5e2277cde4e764
James Shore <jshore@jamesshore.com>
2015-11-21 12:57:21 -0800

    Comment example 'build/scripts' files

--- example/build/scripts/watch.js
+++ example/build/scripts/watch.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
 
 // Automatically runs build when files change.
+// There's no Quixote-specific configuration in this file.
 
 (function() {
 	"use strict";
