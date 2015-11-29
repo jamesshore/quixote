a87805ab5b53a0d95f6625684b9215148cf6f562
woldie <woldie@users.noreply.github.com>
2015-11-29 09:48:45 -0800

    added assertions for comparing ElementClipEdge to ElementEdge in a QElement.assert.
    Tested that a clip style could not be computed for position: relative and led to a ClipNotAppliedException being thrown.

--- src/_assertable_test.js
+++ src/_assertable_test.js
@@ -76,33 +76,83 @@ describe("Assertable abstract base class", function() {
 		}, "a message:\n" + diff + "\n", "different, with a message");
 	});
 
-	it("supports assertions in nested expected with clip object", function() {
-		var domElement = element.toDomElement();
+	describe("clip", function() {
+		it("supports assertions in nested expected with clip object", function() {
+			var domElement = element.toDomElement();
 
-		domElement.setAttribute("style", [
-			"position: absolute",
-			"left: 20px",
-			"top: 10px",
-			"margin-top: 35px",
-			"margin-left: 14px",
-			"margin-bottom: 102px",
-			"margin-right: 900px",
-			"width: 130px",
-			"height: 60px",
-			"clip: rect(5px 120px 45px 20px)",
-			"clip: rect(5px, 120px, 45px, 20px)"
-		].join(";"));
+			domElement.setAttribute("style", [
+				"position: absolute",
+				"left: 20px",
+				"top: 10px",
+				"margin-top: 35px",
+				"margin-left: 14px",
+				"margin-bottom: 102px",
+				"margin-right: 900px",
+				"width: 130px",
+				"height: 60px",
+				"clip: rect(5px 120px 45px 20px)",
+				"clip: rect(5px, 120px, 45px, 20px)"
+			].join(";"));
 
-		element.assert({
-			clip: {
-				top: 5 + 10 + 35,       // clip top + top + margin-top
-				bottom: 45 + 10 + 35,   // clip bottom + top + margin-top
-				left: 20 + 20 + 14,     // clip left + left + margin-left
-				right: 120 + 20 + 14,   // clip right + left + margin-left
-				width: 100,
-				height: 40
-			}
-		}, "nested expected clip object works");
+			element.assert({
+				clip: {
+					top: 5 + 10 + 35,       // clip top + top + margin-top
+					bottom: 45 + 10 + 35,   // clip bottom + top + margin-top
+					left: 20 + 20 + 14,     // clip left + left + margin-left
+					right: 120 + 20 + 14,   // clip right + left + margin-left
+					width: 100,
+					height: 40
+				}
+			}, "nested expected clip object works");
+		});
+
+		it("can compare ElementClipEdge against like-directioned ElementEdges", function() {
+			// bottom of two should be 100
+			var two = frame.add("<div style='position: absolute; top: 25px; margin-top: 35px; height: 40px;'>two</div>");
+
+			// top of element's clip rect should be at 100
+			var domElement = element.toDomElement();
+			domElement.setAttribute("style", [
+				"position: absolute",
+				"top: 60px",
+				"margin-top: 25px",
+				"height: 60px",
+				"clip: rect(15px auto auto auto)",
+				"clip: rect(15px, auto, auto, auto)"
+			].join(";"));
+
+			element.assert({
+				clip: {
+					top: two.bottom
+				}
+			}, "element's clipTop equals the bottom of two");
+		});
+
+		it("will throw a ClipNotAppliedException when asserting clip on an element that is not position: absolute or fixed", function() {
+			var domElement = element.toDomElement();
+
+			domElement.setAttribute("style", [
+				"position: relative",   // clip styles should be ignored because position: relative is used instead of absolute
+				"left: 20px",
+				"top: 10px",
+				"width: 130px",
+				"height: 60px",
+				"clip: rect(5px 120px 45px 20px)",
+				"clip: rect(5px, 120px, 45px, 20px)"
+			].join(";"));
+
+			var ClipNotAppliedException = require("./descriptors/element_clip_edge").ClipNotAppliedException;
+			assert.exception(
+				function() {
+					element.assert({
+						clip: {
+							top: 25       // clip top + top
+						}
+					});
+				},
+				ClipNotAppliedException,
+				"element.assert will fail with a ClipNotAppliedException because a clip style could not be computed");
+		});
 	});
 
 });
