7a57526ba23d62bc6fc5314e83ba8a61011f9338
James Shore <jshore@jamesshore.com>
2015-12-05 11:58:21 -0800

    Upgrade Chrome 46 --> 47

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -9,7 +9,7 @@
 // incorrectly reported as "0.0.0".
 
 module.exports = [
-	"Chrome 46.0.2490 (Mac OS X 10.10.5)",
+	"Chrome 47.0.2526 (Mac OS X 10.10.5)",
 	"Firefox 42.0.0 (Mac OS X 10.10.0)",
 	"IE 8.0.0 (Windows 7 0.0.0)",
 	"IE 9.0.0 (Windows 7 0.0.0)",
