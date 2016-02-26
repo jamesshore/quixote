a4901585f32e92673555f5ae60a7308092d1f4ce
James Shore <jshore@jamesshore.com>
2016-02-25 21:55:16 -0800

    Removed IE 10

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -12,7 +12,6 @@ module.exports = [
 	"Chrome 48.0.2564 (Mac OS X 10.10.5)",
 	"Firefox 44.0.0 (Mac OS X 10.10.0)",
 	"IE 8.0.0 (Windows 7 0.0.0)",
-	"IE 10.0.0 (Windows 7 0.0.0)",
 	"IE 11.0.0 (Windows 7 0.0.0)",
 	"Edge 13.10586.0 (Windows 10 0.0.0)",
 	"Safari 9.0.3 (Mac OS X 10.10.5)",
