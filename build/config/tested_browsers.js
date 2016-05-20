3c646db806ebcae760c940d94466678fa8a8620c
James Shore <jshore@jamesshore.com>
2016-05-20 18:02:55 +0300

    Upgrade iOS 9.2 --> 9.3

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -15,7 +15,7 @@ module.exports = [
 	"IE 11.0.0 (Windows 7 0.0.0)",
 	"Edge 13.10586.0 (Windows 10 0.0.0)",
 	"Safari 9.1.0 (Mac OS X 10.11.4)",
-	"Mobile Safari 9.0.0 (iOS 9.2.0)",
+	"Mobile Safari 9.0.0 (iOS 9.3.0)",
 	"Opera 35.0.2066 (Mac OS X 10.11.4)",
 	"Chrome Mobile 44.0.2403 (Android 6.0.0)"
 ];
