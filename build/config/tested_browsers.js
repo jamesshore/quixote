d4577f1025e54b99f1c7e318c5a0b830775ac2b6
James Shore <jshore@jamesshore.com>
2016-07-14 13:57:17 -0700

    Upgrade Safari 9.1.0 --> 9.1.1

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -14,7 +14,7 @@ module.exports = [
 	"IE 8.0.0 (Windows 7 0.0.0)",
 	"IE 11.0.0 (Windows 7 0.0.0)",
 	"Edge 13.10586.0 (Windows 10 0.0.0)",
-	"Safari 9.1.0 (Mac OS X 10.11.4)",
+	"Safari 9.1.1 (Mac OS X 10.11.5)",
 	"Mobile Safari 9.0.0 (iOS 9.3.0)",
 	"Opera 37.0.2178 (Mac OS X 10.11.5)",
 	"Chrome Mobile 44.0.2403 (Android 6.0.0)"
