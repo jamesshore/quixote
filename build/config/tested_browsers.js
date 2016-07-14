7efd38787accd7949a5f8430f0701d621358f158
James Shore <jshore@jamesshore.com>
2016-07-14 13:56:47 -0700

    Upgrade Firefox 46 --> 47

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -10,7 +10,7 @@
 
 module.exports = [
 	"Chrome 51.0.2704 (Mac OS X 10.11.5)",
-	"Firefox 46.0.0 (Mac OS X 10.11.0)",
+	"Firefox 47.0.0 (Mac OS X 10.11.0)",
 	"IE 8.0.0 (Windows 7 0.0.0)",
 	"IE 11.0.0 (Windows 7 0.0.0)",
 	"Edge 13.10586.0 (Windows 10 0.0.0)",
