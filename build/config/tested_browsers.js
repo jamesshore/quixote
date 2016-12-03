61f2da9e52e5d23466e250864f8c11d72717c150
James Shore <jshore@jamesshore.com>
2016-12-03 10:45:43 -0800

    Upgrade MS Edge 13 --> 14

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -13,7 +13,7 @@ module.exports = [
 	"Firefox 50.0.0 (Mac OS X 10.11.0)",
 	"IE 8.0.0 (Windows 7 0.0.0)",
 	"IE 11.0.0 (Windows 7 0.0.0)",
-	"Edge 13.10586.0 (Windows 10 0.0.0)",
+	"Edge 14.14393.0 (Windows 10 0.0.0)",
 	"Safari 9.1.1 (Mac OS X 10.11.6)",
 	"Mobile Safari 9.0.0 (iOS 9.3.0)",
 	"Opera 37.0.2178 (Mac OS X 10.11.6)",
