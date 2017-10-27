d301050f840958d389f3fd59930f4e7859b31ff1
James Shore <jshore@jamesshore.com>
2017-10-26 22:23:16 -0500

    Remove ViewportSize descriptor (use GenericSize instead)

--- src/q_viewport.js
+++ src/q_viewport.js
@@ -2,23 +2,23 @@
 "use strict";
 
 var ensure = require("./util/ensure.js");
-var ViewportSize = require("./descriptors/viewport_size.js");
 var ViewportEdge = require("./descriptors/viewport_edge.js");
 var Center = require("./descriptors/center.js");
 var Assertable = require("./assertable.js");
+var GenericSize = require("./descriptors/generic_size.js");
 
 var Me = module.exports = function QViewport(frame) {
 	var QFrame = require("./q_frame.js");   // break circular dependency
 	ensure.signature(arguments, [ QFrame ]);
 
 	// properties
-	this.width = ViewportSize.x(frame);
-	this.height = ViewportSize.y(frame);
-
 	this.top = ViewportEdge.top(frame);
 	this.right = ViewportEdge.right(frame);
 	this.bottom = ViewportEdge.bottom(frame);
 	this.left = ViewportEdge.left(frame);
+
+	this.width = GenericSize.create(this.left, this.right, "width of viewport");
+	this.height = GenericSize.create(this.top, this.bottom, "height of viewport");
 
 	this.center = Center.x(this.left, this.right, "center of viewport");
 	this.middle = Center.y(this.top, this.bottom, "middle of viewport");
