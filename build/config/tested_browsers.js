0f7cd1e080df22f0b07550cc23e90501e55ee0ff
James Shore <jshore@jamesshore.com>
2020-04-08 11:18:55 -0700

    Upgrade Chrome Mobile 44.0.2403 --> 74.0.3729

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -5,8 +5,8 @@
 // browser + operating system combinations.
 
 // Version numbers match those reported by Karma. In some cases, the bugfix
-// version number may be incorrectly reported as ".0", and Windows is
-// incorrectly reported as "0.0.0".
+// version number may be incorrectly reported as ".0". Windows and Android
+// are incorrectly reported as "0.0.0".
 
 module.exports = [
 	"Chrome 80.0.3987 (Mac OS X 10.14.6)",
@@ -16,5 +16,5 @@ module.exports = [
 	"Edge 18.18362.0 (Windows 10 0.0.0)",
 	"Safari 13.1.0 (Mac OS X 10.14.6)",
 	"Mobile Safari 13.0.4 (iOS 13.3.0)",
-	"Chrome Mobile WebView 44.0.2403 (Android 6.0.0)"
+	"Chrome Mobile 74.0.3729 (Android 0.0.0)"
 ];
