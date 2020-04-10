4ada204cf918c3beadc4ebcc4358f807f41a09a0
James Shore <jshore@jamesshore.com>
2020-04-10 13:32:03 -0700

    Extract browser feature detection from quixote.js into browser.js

--- src/_quixote_test.js
+++ src/_quixote_test.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2017 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+// Copyright Titanium I.T. LLC.
 "use strict";
 
 var assert = require("./util/assert.js");
@@ -17,68 +17,6 @@ describe("FOUNDATION: Quixote", function() {
 			done(err);
 		});
 		assert.type(frame, QFrame, "createFrame() returns frame object immediately");
-	});
-
-});
-
-describe("FOUNDATION: Browser capability", function() {
-
-	var frame;
-
-	var mobileSafari;
-	var chromeMobile;
-	var ie8;
-	var ie11;
-
-	beforeEach(function() {
-		var userAgent = navigator.userAgent;
-
-		// These user agent strings may be brittle. It's okay because we only use them in the tests. Modify them
-		// as needed to make sure tests match real-world behavior.
-		mobileSafari = userAgent.match(/(iPad|iPhone|iPod touch);/i) !== null;
-		chromeMobile = userAgent.match(/Android/) !== null;
-		ie8 = userAgent.match(/MSIE 8\.0/) !== null;
-		ie11 = userAgent.match(/rv:11\.0/) !== null;
-	});
-
-	it("detects whether browser expands frame to fit size of page", function() {
-		assert.equal(
-			quixote.browser.enlargesFrameToPageSize(),
-			false,
-			"everything should respect frame size"
-		);
-	});
-
-	it("detects whether browser expands size of font when frame is large", function() {
-		assert.equal(
-			quixote.browser.enlargesFonts(),
-			mobileSafari,
-			"everything but Mobile Safari should respect frame size"
-		);
-	});
-
-	it("detects whether browser can detect `clip: auto` value", function() {
-		assert.equal(
-			quixote.browser.misreportsClipAutoProperty(),
-			ie8,
-			"everything but IE 8 should calculate 'clip: auto' properly"
-		);
-	});
-
-	it("detects whether browser computes `clip: rect(auto, ...)` value correctly", function() {
-		assert.equal(
-			quixote.browser.misreportsAutoValuesInClipProperty(),
-			ie11,
-			"everything but IE 11 should calculate clip values properly"
-		);
-	});
-
-	it("detects whether browser rounds off floating-point pixel values", function() {
-		assert.equal(
-			quixote.browser.roundsOffPixelCalculations(),
-			ie8 || ie11,
-			"only IE 8 and IE 11 should round off pixel values"
-		);
 	});
 
 });
---
+++ src/browser.js
@@ -0,0 +1,126 @@
+// Copyright Titanium I.T. LLC.
+"use strict";
+
+var ensure = require("./util/ensure.js");
+var QFrame = require("./q_frame.js");
+var Size = require("./values/size.js");
+
+var FRAME_WIDTH = 1500;
+var FRAME_HEIGHT = 200;
+
+var features = null;
+
+exports.enlargesFrameToPageSize = createDetectionMethod("enlargesFrame");
+exports.enlargesFonts = createDetectionMethod("enlargesFonts");
+exports.misreportsClipAutoProperty = createDetectionMethod("misreportsClipAuto");
+exports.misreportsAutoValuesInClipProperty = createDetectionMethod("misreportsClipValues");
+exports.roundsOffPixelCalculations = createDetectionMethod("roundsOffPixelCalculations");
+
+exports.detectBrowserFeatures = function(callback) {
+	var frame = QFrame.create(document.body, { width: FRAME_WIDTH, height: FRAME_HEIGHT }, function(err) {
+		if (err) {
+			return callback(new Error("Error while creating Quixote browser feature detection frame: " + err));
+		}
+		return detectFeatures(frame, function(err) {
+			frame.remove();
+			return callback(err);
+		});
+	});
+};
+
+function detectFeatures(frame, callback) {
+	try {
+		features = {};
+		features.enlargesFrame = detectFrameEnlargement(frame, FRAME_WIDTH);
+		features.misreportsClipAuto = detectReportedClipAuto(frame);
+		features.misreportsClipValues = detectReportedClipPropertyValues(frame);
+		features.roundsOffPixelCalculations = detectRoundsOffPixelCalculations(frame);
+
+		detectFontEnlargement(frame, FRAME_WIDTH, function(result) {
+			features.enlargesFonts = result;
+			frame.remove();
+			return callback(null);
+		});
+
+	}
+	catch(err) {
+		features = null;
+		return callback(new Error("Error during Quixote browser feature detection: " + err));
+	}
+}
+
+function createDetectionMethod(propertyName) {
+	return function() {
+		ensure.signature(arguments, []);
+		ensure.that(
+			features !== null,
+			"Must call quixote.createFrame() before using Quixote browser feature detection."
+		);
+
+		return features[propertyName];
+	};
+}
+
+function detectFrameEnlargement(frame, frameWidth) {
+	frame.reset();
+
+	frame.add("<div style='width: " + (frameWidth + 200) + "px'>force scrolling</div>");
+	return !frame.viewport().width.value().equals(Size.create(frameWidth));
+}
+
+function detectReportedClipAuto(frame) {
+	frame.reset();
+
+	var element = frame.add("<div style='clip: auto;'></div>");
+	var clip = element.getRawStyle("clip");
+
+	return clip !== "auto";
+}
+
+function detectReportedClipPropertyValues(frame) {
+	frame.reset();
+
+	var element = frame.add("<div style='clip: rect(auto, auto, auto, auto);'></div>");
+	var clip = element.getRawStyle("clip");
+
+	// WORKAROUND IE 8: Provides 'clipTop' etc. instead of 'clip' property
+	if (clip === "" && element.getRawStyle("clip-top") === "auto") return false;
+
+	return clip !== "rect(auto, auto, auto, auto)" && clip !== "rect(auto auto auto auto)";
+}
+
+function detectRoundsOffPixelCalculations(frame) {
+	var element = frame.add("<div style='font-size: 15px;'></div>");
+	var size = element.calculatePixelValue("0.5em");
+
+	if (size === 7.5) return false;
+	if (size === 8) return true;
+	ensure.unreachable("Failure in roundsOffPixelValues() detection: expected 7.5 or 8, but got " + size);
+}
+
+function detectFontEnlargement(frame, frameWidth, callback) {
+	ensure.that(frameWidth >= 1500, "Detector frame width must be larger than screen to detect font enlargement");
+	frame.reset();
+
+	// WORKAROUND IE 8: we use a <div> because the <style> tag can't be added by frame.add(). At the time of this
+	// writing, I'm not sure if the issue is with frame.add() or if IE just can't programmatically add <style> tags.
+	frame.add("<div><style>p { font-size: 15px; }</style></div>");
+
+	var text = frame.add("<p>arbitrary text</p>");
+	frame.add("<p>must have two p tags to work</p>");
+
+	// WORKAROUND IE 8: need to force reflow or getting font-size may fail below
+	// This seems to occur when IE is running in a slow VirtualBox VM. There is no test for this line.
+	var forceReflow = text.offsetHeight;
+
+	// WORKAROUND Safari 8.0.0: timeout required because font is enlarged asynchronously
+	setTimeout(function() {
+		var fontSize = text.getRawStyle("font-size");
+		ensure.that(fontSize !== "", "Expected font-size to be a value");
+
+		// WORKAROUND IE 8: ignores <style> tag we added above
+		if (fontSize === "12pt") return callback(false);
+
+		return callback(fontSize !== "15px");
+	}, 0);
+}
