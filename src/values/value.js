eaee8922a1ea21b417ba30f6b5f101f7cb59d497
James Shore <jshore@jamesshore.com>
2020-04-12 20:50:26 -0700

    Add Value.isCompatibleWith()

--- src/values/value.js
+++ src/values/value.js
@@ -29,25 +29,27 @@ Me.prototype.equals = function equals(that) {
 	return this.diff(that) === "";
 };
 
+Me.prototype.isCompatibleWith = function isCompatibleWith(that) {
+	if (that === null || typeof that !== "object") return false;
+
+	var compatibleTypes = this.compatibility();
+	for (var i = 0; i < compatibleTypes.length; i++) {
+		if (that instanceof compatibleTypes[i]) return true;
+	}
+	return false;
+};
+
 function ensureCompatibility(self, compatible, args) {
 	var arg;
 	for (var i = 0; i < args.length; i++) {   // args is not an Array, can't use forEach
 		arg = args[i];
-		checkOneArg(self, compatible, arg);
+		if (!self.isCompatibleWith(arg)) {
+			var type = typeof arg;
+			if (arg === null) type = "null";
+			if (type === "object") type = oop.instanceName(arg);
+
+			throw new Error(oop.instanceName(self) + " isn't compatible with " + type);
+		}
 	}
 }
 
-function checkOneArg(self, compatible, arg) {
-	var type = typeof arg;
-	if (arg === null) type = "null";
-	if (type !== "object") throwError(type);
-
-	for (var i = 0; i < compatible.length; i++) {
-		if (arg instanceof compatible[i]) return;
-	}
-	throwError(oop.instanceName(arg));
-
-	function throwError(type) {
-		throw new Error(oop.instanceName(self) + " isn't compatible with " + type);
-	}
-}
